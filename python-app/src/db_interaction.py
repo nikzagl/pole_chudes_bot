701189dd6ebea1c3237371af0341b55e008a8e20
Никита Заглумонин <104226191+nikzagl@users.noreply.github.com>
2025-08-17 14:49:10 +0300

    Fixed variables in interaction with db

--- python-app/src/db_interaction.py
+++ python-app/src/db_interaction.py
@@ -1,4 +1,4 @@
-Iimport asyncpg
+import asyncpg
 import os
 
 pg_user=os.environ["POSTGRES_USER"]
@@ -7,7 +7,7 @@ pg_database = os.environ["POSTGRES_DATABASE"]
 
 
 async def update_user(username: str, user_id: str, score: int) -> None:
-    conn = await asyncpg.connect(host="database", port=5432, user=pg_user, database=pg_database, password=pg_password)
+    conn = await asyncpg.connect(host=pg_database, port=5432, user=pg_user, database=pg_database, password=pg_password)
     table = await conn.fetch("SELECT * FROM scores WHERE user_name = $1; ", username)
     if len(table) == 0:
         await conn.execute("INSERT INTO scores (user_name, user_id, score) VALUES ($1, $2, $3)  ;", username, user_id, score)
